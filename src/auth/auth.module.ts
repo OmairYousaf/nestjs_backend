394e237714f591592504541f54d602386a903d1f
Omair Yousaf <omairyousaf143@gmail.com>
2022-10-20 11:58:57 +0500

    ...

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,5 +1,5 @@
 import { Module } from "@nestjs/common";
-import { JwtModule } from "@nestjs/jwt";
+import { JwtModule, JwtService } from "@nestjs/jwt";
 import { AuthController } from "./auth.controller";
 import { AuthService } from "./auth.service";
 // import {JwtModule} from "@nestjs/
@@ -7,6 +7,6 @@ import { AuthService } from "./auth.service";
 @Module({
     imports:[JwtModule.register({})],
     controllers:[AuthController],
-    providers:[AuthService],
+    providers:[AuthService,JwtService],
 })
 export class AuthModule{}
